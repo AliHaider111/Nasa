368eb54d5c9c3ced8d614e6c77eab55280e7ae98
AliHaider03 <ali.haider342111@gmail.com>
2025-07-09 16:44:42 +0500

    first commit

--- src/config/vars.js
+++ src/config/vars.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 require('dotenv-safe').config({
   path: path.join(__dirname, '../../.env'),
-  example: path.join(__dirname, '../../.env.example'),
+  // example: path.join(__dirname, '../../.env.example'),
 });
 
 module.exports = {
