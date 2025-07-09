49cdd665902b902ec0e665831730f415e08db752
AliHaider03 <ali.haider342111@gmail.com>
2025-07-09 16:47:47 +0500

    env example file are removed

--- src/config/vars.js
+++ src/config/vars.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 require('dotenv-safe').config({
   path: path.join(__dirname, '../../.env'),
-  // example: path.join(__dirname, '../../.env.example'),
 });
 
 module.exports = {
